5a7b49846eef90d3f39bb59cdc29ec8ada171d77
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2019-09-16 17:59:33 -0300

    Add model for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(
+        default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
+
+    is_active = models.BooleanField(
+        default=True,
+        help_text=_('Designates whether this user shold be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
