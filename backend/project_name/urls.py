a4323e55cb4a9c30a38b74d166c0f4065c3cb9ef
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2019-11-17 22:28:13 -0300

    Create a simples page to render a itworks message

--- backend/project_name/urls.py
+++ backend/project_name/urls.py
@@ -1,10 +1,17 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path(
+        '',
+        TemplateView.as_view(template_name='myapp/itworks.html'),
+        name='home'
+    ),
 ]
 
 if settings.DEBUG:
