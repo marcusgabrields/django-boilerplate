0c7d6f64202e87101efde03c1920ee726c812e24
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2019-11-17 21:38:58 -0300

    Add inital config to integrate Reac and Django

---
+++ frontend/js/index.js
@@ -0,0 +1,10 @@
+// import pages
+import './bootstrap-includes';
+import '../sass/style.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('react-app'));
---
+++ webpack.base.config.js
@@ -0,0 +1,77 @@
+import path from 'path';
+import BundleTracker from 'webpack-bundle-tracker';
+
+const nodeModulesDir = path.resolve(__dirname, 'node_modules');
+
+module.exports = [
+  {
+    entry: ['./frontend/js/jquery-index.js'],
+    output: {
+      path: path.resolve('./frontend/bundles/'),
+      filename: 'bundle-jquery.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: [nodeModulesDir],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /jquery\/dist\/jquery\.js$/,
+          loader: 'expose-loader?$',
+        },
+        {
+          test: /jquery\/dist\/jquery\.js$/,
+          loader: 'expose-loader?jQuery',
+        },
+      ],
+    },
+    plugins: [
+      new BundleTracker({
+        filename: './jquery-webpack-stats.json',
+      }),
+    ],
+  },
+  {
+    context: __dirname,
+    entry: [
+      // defined in local or prod
+    ],
+    output: {
+      // defined in local or prod
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
+        {
+          test: /\.scss$/,
+          loaders: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(svg)(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=100000',
+        },
+        {
+          test: /\.(jpg|png)?$/,
+          loaders: ['file-loader?name=i-[hash].[ext]'],
+        },
+      ],
+    },
+    plugins: [
+      // defined in local or prod
+    ],
+    resolve: {
+      modules: ['node_modules', 'bower_components', path.resolve(__dirname, 'frontend/js/')],
+      extensions: ['.js', '.jsx'],
+    },
+  },
+];
