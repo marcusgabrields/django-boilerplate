d60db6273a81723a5c031cb8f32efb088515d40f
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-04-21 18:31:56 -0300

    Add webpack confg to prod environment

---
+++ webpack.prod.config.js
@@ -0,0 +1,78 @@
+
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BundleTracker = require('webpack-bundle-tracker');
+const path = require('path');
+
+const baseConfig = require('./webpack.base.config');
+
+const nodeModulesDir = path.resolve(__dirname, 'node_modules');
+
+baseConfig.mode = 'production';
+baseConfig.devtool = 'source-map';
+
+baseConfig.entry = [
+  'whatwg-fetch',
+  '@babel/polyfill',
+  './frontend/js/index.js'
+];
+
+baseConfig.output = {
+  path: path.resolve('./frontend/webpack_bundles/'),
+  publicPath: '',
+  filename: '[name]-[hash].js',
+};
+
+baseConfig.optimization = {
+  minimize: true,
+  splitChunks: {
+    chunks: 'all',
+  },
+}
+
+baseConfig.module.rules.push(
+  {
+    test: /\.jsx?$/,
+    exclude: [nodeModulesDir],
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+      },
+    },
+  },
+  {
+    test: /\.(woff(2)?|eot|ttf)(\?v=\d+\.\d+\.\d+)?$/,
+    use: {
+      loader: 'file-loader',
+      options: {
+        name: 'fonts/[name].[ext]'
+      }
+    }
+  }
+);
+
+baseConfig.plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production'),
+    },
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name]-[hash].css',
+    disable: false,
+    allChunks: true
+  }),
+  new BundleTracker({
+    filename: './webpack-stats.json',
+  }),
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      context: __dirname,
+      postcss: [autoprefixer],
+    },
+  }),
+);
+
+module.exports = baseConfig;
