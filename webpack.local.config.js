ae96cec5fb9c4a7bcbc2f5e603848695c309df6f
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-04-21 18:29:27 -0300

    Adjust webpack local configs

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -1,49 +1,61 @@
-import autoprefixer from 'autoprefixer';
-import webpack from 'webpack';
-import baseConfig from './webpack.base.config';
-import BundleTracker from 'webpack-bundle-tracker';
-import path from 'path';
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker');
+const path = require('path');
 
-const nodeModulesDir = path.resolve(__dirname, 'node_modules');
+const baseConfig = require('./webpack.base.config');
 
-baseConfig[0].mode = 'development';
-baseConfig[1].mode = 'development';
+const nodeModulesDir = path.resolve(__dirname, 'node_modules')
 
-baseConfig[1].entry = [
-  'webpack-dev-server/client?http://localhost:3000',
-  'webpack/hot/only-dev-server',
+baseConfig.mode = 'development';
+
+baseConfig.entry = [
+  'react-hot-loader/patch',
   'whatwg-fetch',
   '@babel/polyfill',
-  './frontend/js/index',
+  './frontend/js/index.js',
 ];
 
-baseConfig[0].output['publicPath'] = 'http://localhost:3000/frontend/bundles/';
-baseConfig[1].output = {
+baseConfig.output = {
   path: path.resolve('./frontend/bundles/'),
   publicPath: 'http://localhost:3000/frontend/bundles/',
   filename: '[name].js',
 };
 
-baseConfig[1].module.rules.push(
+baseConfig.optimization = {
+  splitChunks: {
+    chunks: 'all',
+  },
+};
+
+baseConfig.module.rules.push(
   {
     test: /\.jsx?$/,
     exclude: [nodeModulesDir],
-    loader: require.resolve('babel-loader'),
+    use: {
+      loader: 'babel-loader',
+
+    }
   },
   {
     test: /\.(woff(2)?|eot|ttf)(\?v=\d+\.\d+\.\d+)?$/,
-    loader: 'url-loader?limit=100000',
+    use: {
+      loader: 'url-loader',
+      options: {
+        limit: 100000
+      }
+    }
   }
 );
 
-baseConfig[1].plugins = [
+baseConfig.plugins.push(
   new webpack.EvalSourceMapDevToolPlugin({
-    exclude: /node_modules/,
+    exclude: /node_modules/
   }),
-  new webpack.HotModuleReplacementPlugin(),
   new webpack.NamedModulesPlugin(),
   new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
   new BundleTracker({
+    path: __dirname,
     filename: './webpack-stats.json',
   }),
   new webpack.LoaderOptionsPlugin({
@@ -52,25 +64,6 @@ baseConfig[1].plugins = [
       postcss: [autoprefixer],
     },
   }),
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
-    Tether: 'tether',
-    'window.Tether': 'tether',
-    Popper: ['popper.js', 'default'],
-    Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
-    Button: 'exports-loader?Button!bootstrap/js/dist/button',
-    Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
-    Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
-    Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
-    Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
-    Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
-    Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
-    Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
-    Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
-    Util: 'exports-loader?Util!bootstrap/js/dist/util',
-  }),
-];
+);
 
 module.exports = baseConfig;
